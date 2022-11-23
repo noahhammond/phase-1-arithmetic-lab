1ff974cd2a625b570a2a00f91e60924d4d884347
Noah <business.noah@hotmail.com>
2022-11-23 14:18:08 -0700

    commit tree 0abb7063955ea766e83bfcc1339b1bd7107210dc

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 3) + 2;
+
+const num3 = 8
+const num4 = 2
+
+const mod = num3 / num4;
+
+const max = Math.max(20);
+
+ 
